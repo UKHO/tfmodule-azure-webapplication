299b9083f3dd80a01f9de887f78dc9c132ed52a7
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2023-09-22 14:46:28 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,43 @@
 variable "name" {
   type = string
-  default = "test"
 }
 
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "app_settings" {
+  type = map(string)
+}
+
+variable "tags" {
+
+}
+
+variable "ip_restrictions" {
+  type = list(any)
+}
+
+variable "custom_domain" {
+  type = string
+}
+
+variable "certificate_name" {
+  type = string
+}
+
+variable "certificate_key_vault_name" {
+  type = string
+}
+
+variable "certificate_key_vault_rg" {
+  type = string
+}
