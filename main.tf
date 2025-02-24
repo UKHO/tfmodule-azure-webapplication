2a66d812885898f428a742f7772672fd2ffd7e58
GhostShaikh <farhan.shaikh@ukho.gov.uk>
2025-02-24 15:17:44 +0000

    Updated Modules, Intergate PE works.

--- main.tf
+++ main.tf
@@ -1,10 +1,71 @@
+data "azurerm_resource_group" "main" {
+  provider = azurerm.spoke
+  name     = var.pe_vnet_rg
+}
+data "azurerm_virtual_network" "main" {
+  provider            = azurerm.spoke
+  name                = var.pe_vnet_name
+  resource_group_name = data.azurerm_resource_group.main.name
+}
+data "azurerm_subnet" "subnet" {
+  provider             = azurerm.spoke
+  name                 = var.pe_subnet_name
+  virtual_network_name = data.azurerm_virtual_network.main.name
+  resource_group_name  = data.azurerm_resource_group.main.name
+}
+data "azurerm_resource_group" "dnsrg" {
+  provider = azurerm.hub
+  name     = var.resource_group_name
+}
+data "azurerm_private_dns_zone" "main" {
+  provider            = azurerm.hub
+  name                = var.dns_zone
+  resource_group_name = data.azurerm_resource_group.dnsrg.name
+}
+resource "azurerm_private_dns_zone_virtual_network_link" "main" {
+  provider              = azurerm.hub
+  name                  = var.vnet_link
+  resource_group_name   = var.resource_group_name
+  private_dns_zone_name = data.azurerm_private_dns_zone.main.name
+  virtual_network_id    = data.azurerm_virtual_network.main.id
+  lifecycle {
+    ignore_changes = [
+    tags
+    ]
+  }
+}
+# locals {
+#   pe_rg_name = var.pe_resource_group.name
+# }
+resource "azurerm_private_endpoint" "main" {
+  provider            = azurerm.spoke
+  name                = "m-${var.pe_identity[count.index]}-${var.pe_environment}-pe"
+  count               = length(var.pe_identity)
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  subnet_id           = data.azurerm_subnet.subnet.id
+  lifecycle {
+    ignore_changes = [
+    tags
+    ]
+  }
+  private_service_connection {
+    name                           = var.network_type
+    private_connection_resource_id = var.private_connection[count.index]
+    is_manual_connection           = false
+    subresource_names              = var.subresource_names
+  }
+  private_dns_zone_group {
+    name                 = var.zone_group
+    private_dns_zone_ids = [data.azurerm_private_dns_zone.main.id]
+  }
+}
 resource "azurerm_user_assigned_identity" "tf" {
   provider            = azurerm.sub
   name                = "appservice-identity"
   location            = var.location
   resource_group_name = var.resource_group_name
 }
-
 resource "azurerm_app_service_plan" "app_service_plan" {
   provider            = azurerm.sub
   name                = "${var.name}-asp"
@@ -12,14 +73,12 @@ resource "azurerm_app_service_plan" "app_service_plan" {
   resource_group_name = var.resource_group_name
   kind                = "Linux"
   reserved            = true
-
   sku {
     tier = "PremiumV3"
     size = "P1v3"
   }
   tags = var.tags
 }
-
 resource "azurerm_app_service" "webapp_service" {
   provider            = azurerm.sub
   name                = var.name
@@ -35,21 +94,16 @@ resource "azurerm_app_service" "webapp_service" {
     ftps_state       = "Disabled"
     ip_restriction   = var.ip_restrictions
   }
-
   depends_on = [
     azurerm_app_service_plan.app_service_plan,
     azurerm_user_assigned_identity.tf
   ]
-
   app_settings = var.app_settings
-
   identity {
-    type = "UserAssigned"
+    type         = "UserAssigned"
     identity_ids = [azurerm_user_assigned_identity.tf.id]
   }
-
 }
-
 resource "azurerm_app_service_slot" "webapp_service_slot_inactive" {
   provider            = azurerm.sub
   name                = "Inactive"
@@ -66,47 +120,39 @@ resource "azurerm_app_service_slot" "webapp_service_slot_inactive" {
     ftps_state       = "Disabled"
     ip_restriction   = var.ip_restrictions
   }
-
   depends_on = [
     azurerm_app_service_plan.app_service_plan,
     azurerm_user_assigned_identity.tf
   ]
-
   app_settings = var.app_settings
-
   identity {
-    type = "UserAssigned"
+    type         = "UserAssigned"
     identity_ids = [azurerm_user_assigned_identity.tf.id]
   }
 }
-
 resource "azurerm_app_service_virtual_network_swift_connection" "webapp_vnet_integration" {
-  provider            = azurerm.sub
+  provider       = azurerm.sub
   app_service_id = azurerm_app_service.webapp_service.id
   subnet_id      = var.subnet_id
 }
-
 resource "azurerm_app_service_slot_virtual_network_swift_connection" "webapp_inactive_slot_vnet_integration" {
-  provider            = azurerm.sub
+  provider       = azurerm.sub
   slot_name      = azurerm_app_service_slot.webapp_service_slot_inactive.name
   app_service_id = azurerm_app_service.webapp_service.id
   subnet_id      = var.subnet_id
 }
-
 // First Read the External Key Vault
 data "azurerm_key_vault" "certskv" {
   provider            = azurerm.ssl
   name                = var.certificate_key_vault_name
   resource_group_name = var.certificate_key_vault_rg
 }
-
 // Now Read the Certificate
 data "azurerm_key_vault_certificate" "kvcert" {
   provider     = azurerm.ssl
   name         = var.certificate_name
   key_vault_id = data.azurerm_key_vault.certskv.id
 }
-
 //Get Certificate from External KeyVault
 resource "azurerm_app_service_certificate" "cert" {
   provider            = azurerm.sub
@@ -115,26 +161,22 @@ resource "azurerm_app_service_certificate" "cert" {
   location            = var.location
   key_vault_secret_id = data.azurerm_key_vault_certificate.kvcert.id
 }
-
 resource "azurerm_app_service_custom_hostname_binding" "domain" {
   provider            = azurerm.sub
-  depends_on = [ azurerm_app_service_certificate.cert ]
-  hostname = var.custom_domain
-  app_service_name = azurerm_app_service.webapp_service.name
+  depends_on          = [azurerm_app_service_certificate.cert]
+  hostname            = var.custom_domain
+  app_service_name    = azurerm_app_service.webapp_service.name
   resource_group_name = var.resource_group_name
-
   # Ignore ssl_state and thumbprint as they are managed using
   # azurerm_app_service_certificate_binding.example
   lifecycle {
     ignore_changes = [ssl_state, thumbprint]
   }
 }
-
 resource "azurerm_app_service_managed_certificate" "tf" {
-  provider            = azurerm.sub
+  provider                   = azurerm.sub
   custom_hostname_binding_id = azurerm_app_service_custom_hostname_binding.domain.id
 }
-
 resource "azurerm_app_service_certificate_binding" "example" {
   provider            = azurerm.sub
   hostname_binding_id = azurerm_app_service_custom_hostname_binding.domain.id
--- variables.tf
+++ variables.tf
@@ -1,43 +1,77 @@
+variable "dns_zone" {
+  description = "alias to create private dns zone - be aware this is dependant on the endpoint"
+  default     = "privatelink.azurewebsites.net"
+}
+variable "vnet_link" {
+  description = "alias of the virtual network link"
+  default     = ""
+}
+variable "location" {
+  default = "uksouth"
+}
+variable "network_type" {
+  description = "type of connection"
+  default     = "network"
+}
+variable "private_connection" {
+  description = "endpoint resource id (for example [/subscriptions/SUBID/resourceGroups/RGNAME/providers/Microsoft.Web/sites/APP_SERVICE_NAME])"
+  type        = list(string)
+  default     = [""]
+}
+variable "zone_group" {
+  description = "private dns zone group"
+  default     = ""
+}
+variable "pe_identity" {
+  description = "identity that will create all the private endpoint resources required"
+  default     = ""
+}
+variable "pe_environment" {
+  description = "environment for private endpoint (for example dev | prd | qa | pre)"
+  default     = ""
+}
+variable "pe_vnet_rg" {
+  description = "this is the rg for the spoke vnet"
+  default     = ""
+}
+variable "pe_vnet_name" {
+  description = "vnet name for the private endpoint"
+  default     = ""
+}
+variable "pe_subnet_name" {
+  description = "subname that the private endpoint will associate"
+  default     = ""
+}
+variable "subresource_names" {
+  description = "array of sub resources, if you require additional subresources please define"
+  default     = ["sites"]
+}
 variable "name" {
   type = string
 }
-
 variable "resource_group_name" {
   type = string
 }
-
-variable "location" {
-  type = string
-}
-
 variable "subnet_id" {
   type = string
 }
-
 variable "app_settings" {
   type = map(string)
 }
-
 variable "tags" {
-
 }
-
 variable "ip_restrictions" {
   type = list(any)
 }
-
 variable "custom_domain" {
   type = string
 }
-
 variable "certificate_name" {
   type = string
 }
-
 variable "certificate_key_vault_name" {
   type = string
 }
-
 variable "certificate_key_vault_rg" {
   type = string
 }
